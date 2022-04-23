9af35581a9660cbf6dc8471ccbd28fb9b74f524d
KevinChia <KevinChia@github.com>
2022-04-23 19:26:55 +0800

    finish ocelot apigateway implementation

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace OcelotApiGw
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostContext,config)=>
+                {
+                    // add the configuration file based on the aspnetcore_environment value.
+                    config.AddJsonFile($"ocelot.{hostContext.HostingEnvironment.EnvironmentName}.json",true,true);
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                }).ConfigureLogging((hostContext, loggingBuilder) =>
+                {
+                    // record route logs
+                    loggingBuilder.AddConfiguration(hostContext.Configuration.GetSection("Logging"));
+                    loggingBuilder.AddConsole();
+                    loggingBuilder.AddDebug();
+
+                });
+    }
+}
